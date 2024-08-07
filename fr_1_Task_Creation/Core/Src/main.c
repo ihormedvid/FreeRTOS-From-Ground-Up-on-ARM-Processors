d8771f3fd1816a0de44885e0c2af766b34fc5609
ihormedvid <igormedvid89@gmail.com>
2024-08-07 17:46:32 +0300

    add first task

--- fr_1_Task_Creation/Core/Src/main.c
+++ fr_1_Task_Creation/Core/Src/main.c
@@ -9,9 +9,16 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-
 int __io_putchar(int ch);
 
+
+void vBlueLedControllerTask(void *pvParameters);
+void vRedLedControllerTask(void *pvParameters);
+void vGreenLedControllerTask(void *pvParameters);
+
+typedef uint32_t TaskPrifiler;
+
+TaskPrifiler BlueTaskProfiler, RedTaskProfiler, GreenTaskProfiler;
 
 int main(void)
 {
@@ -24,16 +31,54 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
 
+  xTaskCreate(vBlueLedControllerTask,
+		  "Blue Led Controller",
+		  100,
+		  NULL,
+		  1, NULL);
+
+  xTaskCreate(vGreenLedControllerTask,
+		  "Green Led Controller",
+		  100,
+		  NULL,
+		  1, NULL);
+
+  xTaskCreate(vRedLedControllerTask,
+ 		  "Red Led Controller",
+ 		  100,
+ 		  NULL,
+ 		  1, NULL);
+
+  vTaskStartScheduler();
 
   while (1)
   {
 
-	  printf("Hello from SMT32\n\r");
 
   }
 
 }
 
+void vBlueLedControllerTask(void *pvParameters){
+
+	while(1){
+		BlueTaskProfiler++;
+	};
+}
+
+void vRedLedControllerTask(void *pvParameters) {
+
+	while(1){
+		RedTaskProfiler++;
+	};
+}
+
+void vGreenLedControllerTask(void *pvParameters){
+
+	while(1){
+		GreenTaskProfiler++;
+	};
+}
 
 int __io_putchar(int ch){
 	HAL_UART_Transmit(&huart2, (uint8_t *)&ch,1,0xFFFF);
