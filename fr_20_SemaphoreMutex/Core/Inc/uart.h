3f8772aabbcd2df90633570c1cdeda68ac6e0e60
ihormedvid <igormedvid89@gmail.com>
2024-08-22 22:31:28 +0300

    add 20 pj work with Semaphore mutex

---
+++ fr_20_SemaphoreMutex/Core/Inc/uart.h
@@ -0,0 +1,15 @@
+/*
+ * uart.h
+ *
+ *  Created on: Aug 21, 2024
+ *      Author: Ihor
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+void USART2_UART_TX_Init(void);
+void USART2_UART_RX_Init(void);
+#include "stm32f4xx_hal.h"
+
+#endif /* INC_UART_H_ */
